ffebd2c993dbf8b1a5c4ca7e90bb2ae7175bb758
unknown <lizzbetgrant1009@gmail.com>
2022-04-04 16:27:26 +0800

    First commit

---
+++ web_lab3_final/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+}
+
+module.exports = {
+  env: {
+    PUBLIC_URL: "https://github.com/Oonoo19/web-programming.git",
+    assetPrefix: './'
+  }
+};
---
+++ web_lab3_final/pages/index.js
@@ -0,0 +1,157 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import { MdEmail } from 'react-icons/md';
+import { BsFillTelephoneFill } from 'react-icons/bs';
+import { AiFillHome } from 'react-icons/ai';
+
+export default function Home() {
+  return (
+    <div className='wrapper'>
+      <div className='left'>
+        <div className='picture'>
+          
+        </div>
+        <div className='nameRole'>
+          <div className='name'>Oyungerel Jargalsaikhan</div>
+          <div className='role'>Role</div>
+        </div>
+        <div className='contactWrapper'>
+          <div className='title'>Contact</div>
+          <div className='contact'>
+            <ul>
+             <li>
+               <div className='liWrap'>
+                 <div className='icon'><BsFillTelephoneFill /></div>
+                 <div className='text'>+976 94141984</div>
+               </div>
+             </li>
+             <li>
+               <div className='liWrap'>
+                 <div className='icon'><MdEmail /></div>
+                 <div className='text'>lizzbetgrant1009@gmail.com</div>
+               </div>
+             </li>
+             <li>
+               <div className='liWrap'>
+                 <div className='icon'><AiFillHome /></div>
+                 <div className='text'>New York, Manhattan, 14th street</div>
+               </div>
+             </li>
+           </ul>
+          </div>
+        </div>
+        <div className='profileWrapper'>
+          <div className='title'>Profile</div>
+          <div className='cont'>A passionate, hardworking, and highly organised graphic designer, who always aims to achieve the best results with a practical get the things done approach. </div>
+        </div>
+      </div>
+      <div className='right'>
+        <div className='expWrapper'>
+          <div className='title'>Work Experience</div>
+          <div className='experience'>
+            <ul>
+              <li>
+                <div className='liWrap'>
+                  <div className='liTitle'>Front-End developer</div>
+                  <div className='nameDate'>
+                    <div className='name'>Microsoft</div> 
+                    <div className='line'>|</div>
+                    <div className='date'>2022-2024</div>
+                  </div>
+                  <div className='cont'>Awarded for Best employee of the year 2023. Quitted because of career change.</div>
+                </div>
+              </li>
+              <li>
+                <div className='liWrap'>
+                  <div className='liTitle'>Graphic designer</div>
+                  <div className='nameDate'>
+                    <div className='name'>Facebook</div> 
+                    <div className='line'>|</div>
+                    <div className='date'>since 2024</div>
+                  </div>
+                  <div className='cont'>Building Horizon Worlds project with the best team in Facebook.</div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='eduWrapper'>
+          <div className='title'>Education</div>
+          <div className='education'>
+            <ul>
+              <li>
+                <div className='liWrap'>
+                  <div className='liTitle'>High school education</div>
+                  <div className='nameDate'>
+                    <div className='name'>GAA first school</div> 
+                    <div className='line'>|</div>
+                    <div className='date'>2008-2020</div>
+                  </div>
+                  <div className='cont'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+                </div>
+              </li>
+              <li>
+                <div className='liWrap'>
+                  <div className='liTitle'>Bachelor degree in software engineering</div>
+                  <div className='nameDate'>
+                    <div className='name'>NUM</div> 
+                    <div className='line'>|</div>
+                    <div className='date'>2020-2023</div>
+                  </div>
+                  <div className='cont'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+                </div>
+              </li>
+              <li>
+                <div className='liWrap'>
+                  <div className='liTitle'>Master degree in computer science</div>
+                  <div className='nameDate'>
+                    <div className='name'>MIT</div> 
+                    <div className='line'>|</div>
+                    <div className='date'>2023-2026</div>
+                  </div>
+                  <div className='cont'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='lanWrapper'>
+          <div className='title'>Languages</div>
+          <div className='languages'>
+            <ul>
+              <li>
+                <div className='lanWrap'>
+                  <div className='lan'>Mongolian</div>
+                  <div className='line'>-</div>
+                  <div className='lanLevel'>Native</div>
+                </div>
+              </li>
+              <li>
+                <div className='lanWrap'>
+                  <div className='lan'>English</div>
+                  <div className='line'>-</div>
+                  <div className='lanLevel'>Advanced</div>
+                </div>
+              </li>
+              <li>
+                <div className='lanWrap'>
+                  <div className='lan'>Japanese</div>
+                  <div className='line'>-</div>
+                  <div className='lanLevel'>Basic</div>
+                </div>
+              </li>
+              <li>
+                <div className='lanWrap'>
+                  <div className='lan'>French</div>
+                  <div className='line'>-</div>
+                  <div className='lanLevel'>Basic</div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
